f1c8078ade3ac23df985e9923d1d48faef0bd952
Williams25 <william.souza.timao@hotmail.com>
2021-04-20 22:43:00 -0300

    criando page de welcome

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,30 +1,42 @@
 import React from 'react'
-import { SafeAreaView, Text, Image, TouchableOpacity } from 'react-native'
+import { SafeAreaView, Text, Image, View } from 'react-native'
 import watering from '../../assets/watering.png'
 import { styles } from './styles'
 import { RectButton } from 'react-native-gesture-handler'
 import { Feather } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
 
 export const Welcome: React.FC = () => {
+  const { navigate } = useNavigation()
+
+  const handleToUserIndentification = () => {
+    navigate('UserIndentification')
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {'\n'}
-        suas plantas de {'\n'}
-        forma fácil
-      </Text>
-      <Image
-        style={styles.image}
-        source={watering} />
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
-        sempre que precisar.
-      </Text>
-      <RectButton
-        style={styles.button}
-      >
-        <Feather name="arrow-right" size={28} color="#fff" />
-      </RectButton>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {'\n'}
+          suas plantas de {'\n'}
+          forma fácil
+        </Text>
+        <Image
+          style={styles.image}
+          source={watering}
+          resizeMode="contain"
+        />
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+          sempre que precisar.
+        </Text>
+        <RectButton
+          style={styles.button}
+          onPress={handleToUserIndentification}
+        >
+          <Feather name="arrow-right" size={28} color="#fff" />
+        </RectButton>
+      </View>
     </SafeAreaView>
   )
 }
