24ce332385b192c8c592863a9117959c29678249
Williams25 <william.souza.timao@hotmail.com>
2021-04-20 22:42:07 -0300

    Adicionando rotas em pilha

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar'
+import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost'
+import AppLoading from 'expo-app-loading'
 import React from 'react'
 import { StyleSheet, Text, SafeAreaView } from 'react-native'
-import { Welcome } from './src/pages'
+import { AppStack } from './src/routes/stack.routes'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  })
+
+  if (!fontsLoaded) return <AppLoading />
+
   return (
     <SafeAreaView style={styles.container}>
-      <Welcome />
+      <AppStack />
     </SafeAreaView>
   );
 }
