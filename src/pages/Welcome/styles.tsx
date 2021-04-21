0f1f86b92780b5ded5255ae4c99a313d87dab775
Williams25 <william.souza.timao@hotmail.com>
2021-04-20 22:43:38 -0300

    criando estilo para a page welcome

--- src/pages/Welcome/styles.tsx
+++ src/pages/Welcome/styles.tsx
@@ -1,27 +1,32 @@
-import { StyleSheet } from 'react-native'
-import colors from '../../../styles/colors'
+import { StyleSheet, Dimensions } from 'react-native'
+import { colors, fonts } from '../../../styles'
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  wrapper: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between'
+    justifyContent: 'space-around',
+    paddingHorizontal: 20
   },
   title: {
     fontSize: 32,
     lineHeight: 38,
     fontWeight: 'bold',
+    fontFamily: fonts.heading,
     textAlign: 'center',
     color: colors.heading,
     marginTop: 38
   },
   image: {
-    width: 292,
-    height: 294
+    width: Dimensions.get('window').width * 7
   },
   subtitle: {
     textAlign: 'center',
     fontSize: 18,
+    fontFamily: fonts.text,
     paddingHorizontal: 20,
     color: colors.heading
   },
@@ -33,5 +38,5 @@ export const styles = StyleSheet.create({
     marginBottom: 10,
     width: 60,
     height: 60
-  }
+  },
 })
