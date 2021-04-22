f6b3df7bc1221c56019e28e3f44cf196a513e874
Williams25 <william.souza.timao@hotmail.com>
2021-04-22 16:02:45 -0300

    ajustes no component button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,21 +1,20 @@
 import React, { ReactNode } from 'react'
 import { StyleProp, ViewStyle, Text, TouchableOpacity } from 'react-native'
-import { RectButton } from 'react-native-gesture-handler'
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler'
 import { styles } from './styles'
 
-interface ButtonProps {
+interface ButtonProps extends RectButtonProps{
   title: string
   style?: StyleProp<ViewStyle>
-  onPress?: () => void
 }
 
-export const Button = ({ style, title, onPress, ...rest }: ButtonProps) => {
+export const Button = ({ style, title, ...rest }: ButtonProps) => {
   return (
-    <RectButton
-      style={[style, styles.button]}
-      {...rest}
-    >
-      <Text style={styles.text} >{title}</Text>
-    </RectButton>
+      <RectButton
+        style={[style, styles.button]}
+        {...rest}
+      >
+        <Text style={styles.text} >{title}</Text>
+      </RectButton>
   )
 }
